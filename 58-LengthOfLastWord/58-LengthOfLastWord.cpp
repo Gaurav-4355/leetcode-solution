85ba646443095e93cf9d2fdc1816fd441bdcfe20
Gaurav pandey <gp875686@gmail.com>
2026-01-28 06:37:10 +0530

    Create 58-LengthOfLastWord

---
+++ 58-LengthOfLastWord/58-LengthOfLastWord.cpp
@@ -0,0 +1,17 @@
+// Last updated: 28/01/2026, 06:37:10
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int l=s.length()-1;
+        int count=0;
+         while(l >= 0 && s[l] == ' ') {
+            l--;
+        }
+
+        while(l >= 0 && s[l]!=' '){
+            count++;
+            l--;
+        }
+        return count;
+    }
+};
